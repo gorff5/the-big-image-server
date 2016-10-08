b4bf53fe790950c3d9e8105b70661e3feba7433e
AsafMainPC <gorff55@gmail.com>
2016-10-08 20:21:25 +0300

    fix socket io not working

--- services/socket.service.js
+++ services/socket.service.js
@@ -1,22 +1,23 @@
-
 var fs = require('fs');
-var http = require('http');
-var socket = require('socket.io')(http,  {origins:'localhost:* http://localhost:*'});
 var imageService = require('./image.service');
 
-socket.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
+var socket={};
 
-    socket.on('message', function(msg){
-        console.log('message: ' + msg);
-        socket.emit('chat message', base64_encode('./public/images/sample-image.jpg'));
-        imageService.cropImage('./public/images/sample-image.jpg')
-    });
+function connect(io) {
+    io.on('connection', function (socket) {
+        console.log('a user connected');
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
 
-});
+        socket.on('share', function (msg) {
+            console.log('message: ' + msg);
+            io.emit('displayImage', base64_encode('./public/images/sample-image.jpg'));
+            imageService.cropImage('./public/images/sample-image.jpg')
+        });
+
+    });
+}
 
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
@@ -26,4 +27,7 @@ function base64_encode(file) {
     return new Buffer(bitmap).toString('base64');
 }
 
-module.exports = socket;
+module.exports =function(io){
+    connect(io);
+    return socket;
+};
